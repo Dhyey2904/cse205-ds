f993ab8ffe3d39428069bca1577f884a278b9d88
Dhyey <dhyey29045@gmail.com>
2023-09-03 16:35:24 +0530

    lab-3

--- assignments/28.08.2023/2221.java
+++ assignments/28.08.2023/2221.java
@@ -1,3 +1,8 @@
 class S2221 {
-  
+  public int triangularSum(int[] nums) {
+    for (int sz = nums.length; sz > 0; sz--)
+      for (int i = 0; i + 1 < sz; i++)
+        nums[i] = (nums[i] + nums[i + 1]) % 10;
+    return nums[0];        
+  }
 }
